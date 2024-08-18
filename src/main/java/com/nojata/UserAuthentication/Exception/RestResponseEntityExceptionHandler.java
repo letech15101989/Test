e3c796684b5433d2883f0f6c0876f31feb86f8c7
LeTech Oy <140951063+letech15101989@users.noreply.github.com>
2024-08-18 15:51:46 +0200

    Update RestResponseEntityExceptionHandler.java

--- src/main/java/com/nojata/UserAuthentication/Exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/nojata/UserAuthentication/Exception/RestResponseEntityExceptionHandler.java
@@ -5,7 +5,7 @@
  */
 package com.nojata.UserAuthentication.Exception;
 
-import org.omg.CORBA.UserException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
